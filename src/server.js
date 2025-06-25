bbb0c0d75999acdf2ce4a8dda548b898eafd5919
Esakkipandi0214 <praveenpandian01@gmail.com>
2025-06-25 09:40:59 +0530

    hjbknlm

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 const app = require('./app');
 
-// // Define Port
-// const PORT = process.env.PORT || 3000;
+// Define Port
+const PORT = process.env.PORT || 3000;
 
-// // Start Server
-// app.listen(PORT, () => {
-//   console.log(`Server is running on http://localhost:${PORT}`);
-// });
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
