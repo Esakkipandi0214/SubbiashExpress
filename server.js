f4b73f5be09f75cd5a0c8ec02da099f9bc042442
Esakkipandi0214 <praveenpandian01@gmail.com>
2025-06-25 17:28:19 +0530

    cfhgvhjbkjnmk

--- server.js
+++ server.js
@@ -1,4 +1,48 @@
-const app = require('./src/app');
+// const app = require('./src/app');
+
+
+
+// app.js
+const express = require('express');
+require('dotenv').config();
+
+const connectDB = require('./src/DBConfig/db');
+connectDB();
+
+const app = express();
+const cors = require('cors');
+
+const actionsRouter = require('./src/routes/actions');
+const actionsRouterUser = require('./src/routes/user');
+const todoRoutes = require('./src/routes/todoRoutes');
+
+// Middleware
+app.use(cors({
+  origin: ['https://agenda-verse-build.vercel.app', 'http://localhost:8080'],
+  credentials: true
+}));
+app.use(express.json());
+
+// Routes
+app.use('/api/actions', actionsRouter);
+app.use('/api/userActions', actionsRouterUser);
+app.use('/api/todos', todoRoutes);
+
+// Root Route
+app.get('/', (req, res) => {
+  res.send('Welcome to the Simple RESTful API!');
+});
+
+// 404 Handler
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not Found' });
+});
+
+// Error Handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Something went wrong!' });
+});
 
 // Define Port
 const PORT = process.env.PORT || 3000;
